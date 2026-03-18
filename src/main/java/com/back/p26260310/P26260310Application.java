f0aa90a0d273a440b8b31186680c0ce00411d13e
suuuu9 <asb040923@gmail.com>
2026-03-18 11:24:03 +0900

    init

---
+++ src/main/java/com/back/p26260310/P26260310Application.java
@@ -0,0 +1,13 @@
+package com.back.p26260310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P26260310Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P26260310Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/p26260310/P26260310ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.p26260310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P26260310ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
